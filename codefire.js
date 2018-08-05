ad19dd87358c14b8a93d1504d7c93a20e2b3372e
Abu-Bakr Adam <asiddik5@gmail.com>
2018-08-05 17:42:47 +0100

    Added codefire entry file

---
+++ codefire.js
@@ -0,0 +1,17 @@
+var util = require('util');
+var fire = require('./index.js');
+
+function CodeFire() {
+    this.version = '\n\t\t\t version: 1.0.3';
+}
+
+CodeFire.prototype.greetings = function(){
+    // say my greetings
+    fire.greet();
+    // console.log(this.version)
+}
+
+var codefire = new CodeFire();
+codefire.greetings();
+
+// console.log('\n\n')
